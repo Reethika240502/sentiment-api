a6c55768a74f58ffd513a40894794f6d7d3dcb2f
Reethika <reethikatekumatla123@gmail.com>
2025-07-21 19:07:52 -0500

    Add CORS middleware and async to /predict endpoint

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from textblob import TextBlob
+from fastapi.middleware.cors import CORSMiddleware  # <-- import this
 
 app = FastAPI()
+
+# Add allowed origins here — include your GitHub Pages URL
+origins = [
+    "https://reethika240502.github.io",
+    "http://localhost",
+    "http://localhost:8000",
+    "https://sentiment-api-0mz2.onrender.com",
+]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # allow frontend origin(s)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class SentimentRequest(BaseModel):
     text: str
@@ -12,7 +30,7 @@ def read_root():
     return {"message": "Sentiment API is running!"}
 
 @app.post("/predict")
-def predict_sentiment(request: SentimentRequest):
+async def predict_sentiment(request: SentimentRequest):
     analysis = TextBlob(request.text)
     polarity = analysis.sentiment.polarity
 
